b3b5e3fd5618c1b181498365b88c2ff7cb48deec
hiimsajo <hiimsajo@gmail.com>
2023-06-16 05:06:02 +0900

    feat: Be answer부분 새 브랜치 생성, 오류수정, mapstruct 적용, 서비스로직은 없음

---
+++ back/build/generated/sources/annotationProcessor/java/main/com/example/back/answer/mapper/AnswerMapperImpl.java
@@ -0,0 +1,60 @@
+package com.example.back.answer.mapper;
+
+import com.example.back.answer.dto.AnswerPatchDto;
+import com.example.back.answer.dto.AnswerPostDto;
+import com.example.back.answer.dto.AnswerResponseDto;
+import com.example.back.answer.entity.Answer;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-16T04:17:00+0900",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.1.jar, environment: Java 11.0.18 (Azul Systems, Inc.)"
+)
+@Component
+public class AnswerMapperImpl implements AnswerMapper {
+
+    @Override
+    public Answer answerPostDtoToAnswer(AnswerPostDto answerPostDto) {
+        if ( answerPostDto == null ) {
+            return null;
+        }
+
+        Answer answer = new Answer();
+
+        answer.setContent( answerPostDto.getContent() );
+
+        return answer;
+    }
+
+    @Override
+    public Answer answerPatchDtoToAnswer(AnswerPatchDto answerPatchDto) {
+        if ( answerPatchDto == null ) {
+            return null;
+        }
+
+        Answer answer = new Answer();
+
+        answer.setAnswerId( answerPatchDto.getAnswerId() );
+        answer.setContent( answerPatchDto.getContent() );
+
+        return answer;
+    }
+
+    @Override
+    public AnswerResponseDto answerToAnswerResponseDto(Answer answer) {
+        if ( answer == null ) {
+            return null;
+        }
+
+        AnswerResponseDto answerResponseDto = new AnswerResponseDto();
+
+        if ( answer.getAnswerId() != null ) {
+            answerResponseDto.setAnswerId( answer.getAnswerId() );
+        }
+        answerResponseDto.setContent( answer.getContent() );
+
+        return answerResponseDto;
+    }
+}
