fcfba933ff3ff57ae71e40b224090d540e11aa3c
jungo <81670100+jungo0@users.noreply.github.com>
2023-06-15 11:47:45 +0900

    feat: 오른쪽 sidebar

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -1,8 +1,10 @@
 import Footer from "../../share/Footer";
+import Sidebar from "../../share/Sidebar";
 export default function Main() {
   return (
     <div>
       메인 페이지
+      <Sidebar />
       <Footer />
     </div>
   );
--- client/src/pages/questions/Ask.jsx
+++ client/src/pages/questions/Ask.jsx
@@ -1,3 +1,9 @@
+import Footer from "../../share/Footer";
 export default function Ask() {
-  return <div>질문작성 페이지</div>;
+  return (
+    <div>
+      질문작성 페이지
+      <Footer />
+    </div>
+  );
 }
