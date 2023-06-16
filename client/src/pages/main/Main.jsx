f9d63f4a48414dd73347a6df8aeaa7057d7fc05e
hahagarden <tingyuan413@gmail.com>
2023-06-16 09:45:29 +0900

    feat: 메인 페이지 상단 구현

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -3,8 +3,20 @@ import styled from "styled-components";
 import Layout from "../../share/Layout";
 import QuestionListItem from "../../components/question/QuestionListItem";
 import { dummyQuestions } from "../../dummy/dummyQuestions";
+import AskButton from "../../share/AskButton";
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 16px;
+`;
+
+const PageHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PageTitle = styled.h1`
+  font-size: 27px;
+`;
 
 const QuestionList = styled.div``;
 
@@ -14,6 +26,10 @@ export default function Main() {
       메인 페이지
       <Layout />
       <Container>
+        <PageHeader>
+          <PageTitle>All Questions</PageTitle>
+          <AskButton />
+        </PageHeader>
         <QuestionList>
           {dummyQuestions.map((question) => (
             <QuestionListItem key={question.Question_id} item={question} />
