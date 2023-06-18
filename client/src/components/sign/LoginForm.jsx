3d9036ebdc45c06bd1903a1899d93da5d69e9854
jungo <81670100+jungo0@users.noreply.github.com>
2023-06-18 19:29:01 +0900

    feat: EmailForm

--- client/src/components/sign/LoginForm.jsx
+++ client/src/components/sign/LoginForm.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import EmailForm from "./EmailForm";
 import PasswordForm from "./PasswordForm";
-import LoginButton from "./LoginButtons";
+import LoginButton from "./LoginButton";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
