92cbd1ec4da95f1cf80afcd62a216d51ce6270ee
j6d9s <qxdo19@gmail.com>
2023-06-19 18:02:49 +0900

    fadfasdfasdfasdfasdfsad

--- back/src/main/java/com/example/back/BackApplication.java
+++ back/src/main/java/com/example/back/BackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(fdmklfmalkfmaklsd3)
 public class BackApplication {
 
 	public static void main(String[] args) {
