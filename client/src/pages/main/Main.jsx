db34f4007a34e54f008c87514b2da256e5d57239
jungo <81670100+jungo0@users.noreply.github.com>
2023-06-21 18:00:12 +0900

    feat:header token 기능 추가

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import AskButton from "../../share/AskButton";
 import QuestionList from "../../components/main/questions/QuestionList";
 import { dummyQuestions } from "../../dummy/dummyQuestions";
-
 export default function Main() {
   return (
     <Container>
