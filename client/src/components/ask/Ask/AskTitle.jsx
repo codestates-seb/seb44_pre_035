fe12b4b1de1f01628741c482518466b9a81a172e
Xiel0112 <guswl000112@gmail.com>
2023-06-21 21:43:44 +0900

    styled: 컴포넌트 함수 변경

--- client/src/components/ask/Ask/AskTitle.jsx
+++ client/src/components/ask/Ask/AskTitle.jsx
@@ -1,15 +1,14 @@
 import styled from "styled-components";
 import PageTitle from "../PageTitle";
 
-export default function AskTitle = ({ title }) => {
+export default function AskTitle({ title }) {
   return (
     <Wrapper>
       <PageTitle title={title} />
     </Wrapper>
   );
-};
+}
 
 const Wrapper = styled.div`
   /* border: 1px solid red; */
 `;
-
