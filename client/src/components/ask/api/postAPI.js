848ad354d641f3df8245183870411b0bf595aea6
Xiel0112 <guswl000112@gmail.com>
2023-06-22 11:08:39 +0900

    feat: 추가 question edit 기능

--- client/src/components/ask/api/postAPI.js
+++ client/src/components/ask/api/postAPI.js
@@ -4,14 +4,6 @@ export const postQuestion = async (body) => {
   await axios.post("/questions/ask", body);
 };
 
-export const EditQuestion = async (body) => {
-  await axios
-    .put("/questions/{question-id}", body)
-    .then((res) => {
-      console.log(res.data);
-      alert("Edit successful :)");
-    })
-    .catch((err) => {
-      alert(err.message, "Edit failed :(");
-    });
+export const EditQuestion = async (questionId, body) => {
+  await axios.patch(`/questions/${questionId}`, body);
 };
